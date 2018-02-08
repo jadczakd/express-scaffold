fc9ae012a02864ec1fbc38b161339cb1418b877d
Dan <jadczak.d@gmail.com>
2018-02-09 00:46:20 +0100

    dj added winston and morgan for logging, file renaming, utils added

--- src/router.js
+++ src/router.js
@@ -4,9 +4,5 @@ import routes from './api/routes'
 const home = Router()
 
 home.use(routes)
-// Catch unhandler errors
-home.use((err, req, res) => {
-  res.status(500).send(err)
-})
 
 export default home
--- src/server.js
+++ src/server.js
@@ -3,19 +3,27 @@ import bodyParser from 'body-parser'
 import db from './config/db'
 import routes from './router'
 import config from './config'
+import { logger } from './utils'
+import morgan from 'morgan'
 
 const app = express()
 // allow for JSON formatted requests
 app.use(bodyParser.json())
+// setup logging
+app.use(morgan('combined', {stream: logger.stream}))
 app.listen(config.port, () => {
-  console.log(`app listening on ${config.port}`)
+  logger.info(`app listening on ${config.port}`)
 })
 // including the predefined routes
 app.use(routes)
+// Catch unhandler errors
+// app.use((err, req, res) => {
+//   res.status(500).send(err)
+// })
 // Uncaught exceptions
-process.on('uncaughtException', err => console.error('Uncaught excepotion:', err))
+process.on('uncaughtException', err => logger.error('Uncaught excepotion:', err))
 // unhandled promises
-process.on('unhandlerdRejection', err => console.error('unhandled rejection:', err))
+process.on('unhandlerdRejection', err => logger.error('unhandled rejection:', err))
 if (process.env.NODE_ENV !== 'test') {
   // connect to db
   db.connect()
